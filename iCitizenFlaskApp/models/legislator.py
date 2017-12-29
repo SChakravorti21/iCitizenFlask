38d9a202ad561f874ad84b76c22c9f4880638d0a
Pranay Neelagiri <pneeldam@gmail.com>
2017-12-29 17:54:26 -0500

    now can create legislator objects from parsed data

--- iCitizenFlaskApp/models/legislator.py
+++ iCitizenFlaskApp/models/legislator.py
@@ -1,6 +1,8 @@
+from pygeocoder import Geocoder
+
 class Legislator(object):
 
-    def __init__(self, level, firstName, lastName, state, chamber, id, party, streetAddress, city, stateAddress, zipcode, phone):
+    def __init__(self, level, firstName, lastName, state, chamber, id, party, district=None):
         self.level = level
         self.firstName = firstName
         self.lastName = lastName
@@ -8,13 +10,8 @@ class Legislator(object):
         self.chamber = chamber
         self.id = id
         self.party = party
-        self.streetAddress = streetAddress
-        self.city = city
-        self.stateAddress = stateAddress
-        self.zipcode = zipcode
-        self.phone = phone
 
-    def printLegislator(self):
+    def print_info(self):
         print(str(self.level) + "\n")
         print(str(self.firstName) + "\n")
         print(str(self.lastName) + "\n")
@@ -22,10 +19,71 @@ class Legislator(object):
         print(str(self.chamber) + "\n")
         print(str(self.id) + "\n")
         print(str(self.party) + "\n")
-        print(str(self.streetAddress) + "\n")
-        print(str(self.city) + "\n")
-        print(str(self.stateAddress) + "\n")
-        print(str(self.zipcode) + "\n")
-        print(str(self.phone) + "\n")
+
+    @classmethod
+    def get_national_legislators(cls, address, city, state, zipcode):
+        legislators = []
+        fullAddress = address + ", " + city + ", " + state + " " + zipcode
+        google_params = {"address": fullAddress, "includeOffices": True, "levels": "country", "roles": ["legislatorLowerBody","legislatorUpperBody"], "key": "AIzaSyBLns0lxH3J7iMYIMaWCUtOX5lwsKhdBd4"}
+        google_response = requests.get(google_base+"representatives/", google_params)
+        google_data = json.loads(google_response.text)
+
+        offices = google_data['offices']
+        districtId = offices[1]['divisionId'][36:]
+        
+        national_params = {"X-API-Key": "Fl38VvBXk8EFlAmEcG4N0Wq1hi6YPnyzi5YODT9k"}
+        national_senate_response = requests.get(national_base+"members/senate/{}/current.json".format(state), headers=national_params)
+
+        national_senate_data = json.loads(national_senate_response.text)
+
+        senators = national_senate_data['results']
+        for senator in senators:
+            level = "national"
+            first_name = senator['first_name']
+            last_name = senator['last_name']
+            chamber = "Senate"
+            id = senator['id']
+            party = senator['party']
+            legislators.append(cls(level, first_name, last_name, state, chamber, id, party))
+
+
+        national_house_response = requests.get(national_base+"members/house/{}/{}/current.json".format(state, districtId), headers=national_params)
+
+        national_house_data = json.loads(national_house_response.text)
+
+        representatives = national_house_data['results']
+        for rep in representatives:
+            level = "national"
+            first_name = rep['first_name']
+            last_name = rep['last_name']
+            chamber = "House"
+            id = rep['id']
+            party = rep['party']
+            legislators.append(cls(level, first_name, last_name, state, chamber, id, party, districtId))
+
+        return legislators
+
+    @classmethod
+    def get_state_legislators(cls, address, city, state, zipcode):
+        legislators = []
+        fullAddress = address + ", " + city + ", " + state + " " + zipcode
+        location =  Geocoder.geocode(fullAddress)
+        state_params = {"lat":location.latitude, "long":location.longitude, "apikey": "70e23970-e0b4-409b-a7d2-3e34f6b88905"}
+
+        state_legislative_response = requests.get(state_base+"legislators/geo/", state_params)
+        state_legislative_data = json.loads(state_legislative_response.text)
+
+        for rep in state_legislative_data:
+            level = "state"
+            first_name = rep['first_name']
+            last_name = rep['last_name']
+            chamber = rep['chamber']
+            id = rep['id']
+            party = rep['party']
+            district = rep['district']
+            legislators.append(cls(level, first_name, last_name, state, chamber, id, party, district))
+
+        return legislators
+
 
 
--- iCitizenFlaskApp/test_scripts/legislator_search.py
+++ iCitizenFlaskApp/test_scripts/legislator_search.py
@@ -1,15 +1,48 @@
-import pyopenstates as states
+import requests, json
 from congress import Congress as national
-import requests
+from pygeocoder import Geocoder
 
-states.set_api_key('70e23970-e0b4-409b-a7d2-3e34f6b88905')
-cong = national('Fl38VvBXk8EFlAmEcG4N0Wq1hi6YPnyzi5YODT9k')
+google_base = "https://www.googleapis.com/civicinfo/v2/"
+state_base = "https://openstates.org/api/v1/"
+national_base = "https://api.propublica.org/congress/v1/"
 
-base = "https://www.googleapis.com/civicinfo/v2/"
+def getLegislators(address, city, state, zipcode):
+    fullAddress = address + ", " + city + ", " + state + " " + zipcode
+    google_params = {"address": fullAddress, "includeOffices": True, "levels": "country", "roles": ["legislatorLowerBody","legislatorUpperBody"], "key": "AIzaSyBLns0lxH3J7iMYIMaWCUtOX5lwsKhdBd4"}
+    google_response = requests.get(google_base+"representatives/", google_params)
+    google_data = json.loads(google_response.text)
 
-def getLegislators(address):
-    params = {"address": address, "key": "AIzaSyBLns0lxH3J7iMYIMaWCUtOX5lwsKhdBd4"}
-    response = requests.get(base+"representatives/", params)
-    print(response.content)
+    offices = google_data['offices']
+    districtId = offices[1]['divisionId'][36:]
+    
+    national_params = {"X-API-Key": "Fl38VvBXk8EFlAmEcG4N0Wq1hi6YPnyzi5YODT9k"}
+    national_senate_response = requests.get(national_base+"members/senate/{}/current.json".format(state), headers=national_params)
 
-getLegislators("17 Queensboro Terrace, East Windsor, NJ 08520")
+    national_senate_data = json.loads(national_senate_response.text)
+
+    senators = national_senate_data['results']
+    for senator in senators:
+        print(senator['name'])
+
+    national_house_response = requests.get(national_base+"members/house/{}/{}/current.json".format(state, districtId), headers=national_params)
+
+    national_house_data = json.loads(national_house_response.text)
+
+    representatives = national_house_data['results']
+    for rep in representatives:
+        print(rep['name'])
+
+    location =  Geocoder.geocode(fullAddress)
+    state_params = {"lat":location.latitude, "long":location.longitude, "apikey": "70e23970-e0b4-409b-a7d2-3e34f6b88905"}
+
+    state_legislative_response = requests.get(state_base+"legislators/geo/", state_params)
+    state_legislative_data = json.loads(state_legislative_response.text)
+
+    for rep in state_legislative_data:
+        print(rep['full_name'])
+
+
+#Legislator.get_national_legislators("17 Queensboro Terrace", "East Windsor",  "NJ",  "08520").print_info()
+#getLegislators("17 Queensboro Terrace", "East Windsor",  "NJ",  "08520")
+#getStateBills("nj", "Crime")
+
