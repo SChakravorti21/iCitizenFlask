76e705d6170c90c490959bb947c84f57e4fa4ecc
SChakravorti21 <shoumyoarchit@gmail.com>
2017-12-30 14:23:21 -0500

    Preferences routes and updating working

--- iCitizenFlaskApp/forms.py
+++ iCitizenFlaskApp/forms.py
@@ -1,4 +1,5 @@
-from wtforms import Form, StringField, PasswordField, validators
+from wtforms import Form, StringField, PasswordField, SelectField, SelectMultipleField, RadioField, validators
+from iCitizenFlaskApp.data import STATES, SUBJECTS
 
 class RegisterForm(Form):
 	name = StringField('Name', [validators.Length(min=1, max=50)])
@@ -7,16 +8,26 @@ class RegisterForm(Form):
 		validators.Email()
 	])
 	username = StringField('Username', [
-		validators.DataRequired(),
+		validators.InputRequired(),
 		validators.Length(min=6, max=25)
 	])
 	password = PasswordField('Password', [
-		validators.DataRequired(),
+		validators.InputRequired(),
 		validators.Length(min=6, max=25)
 	])
 	confirm = PasswordField('Confirm Password', 
 		[validators.EqualTo('password', message='Passwords do not match!')])
 
 class LoginForm(Form):
-	username = StringField('Username', [validators.DataRequired()])
-	password = PasswordField('Password', [validators.DataRequired()])
+	username = StringField('Username', [validators.InputRequired()])
+	password = PasswordField('Password', [validators.InputRequired()])
+
+class PreferencesForm(Form):
+	political_party = RadioField('Political Party', [validators.InputRequired()],
+		choices=[('republican', 'Republican'), ('democrat', 'Democrat'), ('independent', 'Independent')])
+	address = StringField('Address', [validators.InputRequired()])
+	city = StringField('City', [validators.InputRequired()])
+	state = SelectField('State', [validators.InputRequired()],
+		choices=STATES)
+	country = StringField('Country', [validators.InputRequired()])
+	zipcode = StringField('Zip Code', [validators.InputRequired()])
--- iCitizenFlaskApp/views/user_routes.py
+++ iCitizenFlaskApp/views/user_routes.py
@@ -3,9 +3,10 @@ from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from passlib.hash import sha256_crypt
 from functools import wraps
 
-from iCitizenFlaskApp.forms import RegisterForm, LoginForm
+from iCitizenFlaskApp.forms import RegisterForm, LoginForm, PreferencesForm
 
-from iCitizenFlaskApp.dbconfig import db
+from iCitizenFlaskApp.dbconfig import db, QueryKeys
+from iCitizenFlaskApp.data import SUBJECTS
 
 mod = Blueprint('users', __name__)
 
@@ -24,10 +25,11 @@ def register():
 
 		users = db['users']
 		insert_id = users.insert_one({
-			'name': name,
-			'email': email,
-			'username': username,
-			'password': password
+			QueryKeys.NAME: name,
+			QueryKeys.EMAIL: email,
+			QueryKeys.USERNAME: username,
+			QueryKeys.PASSWORD: password,
+			QueryKeys.INPUTTED_PREFERENCES: False
 		}).inserted_id
 
 		print(str(insert_id))
@@ -58,7 +60,7 @@ def login():
 			flash('Invalid username or password', 'danger')
 		else:
 			session['logged_in'] = True
-			session['username'] = username
+			session[QueryKeys.USERNAME] = username
 
 			flash('You have successfully logged in!', 'success')
 			return redirect( url_for('index'))
@@ -69,11 +71,89 @@ def login():
 @mod.route('/logout/', methods=['GET'])
 def logout():
 	session['logged_in'] = False
-	session['username'] = None
+	session[QueryKeys.USERNAME] = None
 
 	flash('You have successfully logged out!', 'success')
 	return redirect( url_for('index'))
 
 @mod.route('/dashboard/', methods=['GET'])
 def load_dashboard():
+	# Check if preferences have been inputted
+	query = {QueryKeys.USERNAME: session[QueryKeys.USERNAME]}
+	users = db['users']
+
+	user = users.find_one(query)
+	print(user[QueryKeys.INPUTTED_PREFERENCES])
+	if not user[QueryKeys.INPUTTED_PREFERENCES]:
+		# Redirect to input those preferences
+
+		return redirect( url_for('users.update_preferences') )
+
 	return render_template('dashboard.html')
+
+@mod.route('/update-prefs/', methods=['GET', 'POST'])
+def update_preferences():
+	form = PreferencesForm(request.form)
+	topics = []
+	error = None
+
+	# Prefill the preferences if the user has set them before
+	query = {QueryKeys.USERNAME: session[QueryKeys.USERNAME]}
+	users = db['users']
+	user = users.find_one(query)
+
+	if user[QueryKeys.INPUTTED_PREFERENCES]:
+		location = user[QueryKeys.LOCATION]
+
+		form.address.data = location[QueryKeys.ADDRESS]
+		form.city.data = location[QueryKeys.CITY]
+		form.state.data = location[QueryKeys.STATE]
+		form.country.data = location[QueryKeys.COUNTRY]
+		form.zipcode.data = location[QueryKeys.ZIPCODE]
+
+		prefs = user[QueryKeys.PREFERENCES]
+		form.political_party.data = prefs[QueryKeys.PARTY]
+		topics = prefs[QueryKeys.TOPICS]
+
+
+	if request.method == 'POST':
+		topics = request.form.getlist('subjects')
+		if not topics or len(topics) == 0:
+			error = 'Please choose at least one topic so that we can find bills that are relevant for you'
+			pass
+
+		if form.validate():
+			party = form.political_party.data
+			address = form.address.data
+			city = form.city.data
+			state = form.state.data
+			country = form.country.data
+			zipcode = form.zipcode.data
+
+			address_obj = {
+				QueryKeys.ADDRESS: address,
+				QueryKeys.CITY: city,
+				QueryKeys.STATE: state,
+				QueryKeys.COUNTRY: country,
+				QueryKeys.ZIPCODE: zipcode,
+			}
+
+			preferences_obj = {
+				QueryKeys.PARTY: party,
+				QueryKeys.TOPICS: topics
+			}
+
+			print(address_obj)
+			print(preferences_obj)
+
+			user = users.find_one_and_update(query, {'$set':
+				{
+					QueryKeys.INPUTTED_PREFERENCES: True,
+					QueryKeys.PREFERENCES: preferences_obj,
+					QueryKeys.LOCATION: address_obj
+				}})
+			print(user)
+
+	return render_template('update_preferences.html', form=form, 
+		subjects=[(str(subject).strip().lower(), subject) for subject in SUBJECTS], 
+		selected_subjects=topics, error=error)
