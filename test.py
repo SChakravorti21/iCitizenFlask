0b5cb08561ca0f725e87a79c7907d31d0e1126fd
Pranay Neelagiri <pneeldam@gmail.com>
2017-12-30 16:32:17 -0500

    implemented state bill fetching

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
 from iCitizenFlaskApp.models.bill import Bill
 from iCitizenFlaskApp.models.legislator import Legislator
                     
-legislators = Legislator.get_national_legislators("17 Queensboro Terrace", "East Windsor", "NJ", "08520")
-bills = Bill.get_national_bills(legislators, ["meat"])                
+#legislators = Legislator.get_national_legislators("17 Queensboro Terrace", "East Windsor", "NJ", "08520")
+#bills = Bill.get_national_bills(legislators, ["meat"])
+
+state_legislators = Legislator.get_state_legislators("17 Queensboro Terrace", "East Windsor", "NJ", "08520")
+Bill.get_state_bills(state_legislators, ["Crime"], "NJ")                
