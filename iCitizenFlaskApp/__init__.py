378c54b091b6144d3acd442003288bf293ee8e55
SChakravorti21 <shoumyoarchit@gmail.com>
2017-12-31 13:25:41 -0500

    Made login persistent

--- iCitizenFlaskApp/__init__.py
+++ iCitizenFlaskApp/__init__.py
@@ -1,6 +1,14 @@
 from flask import Flask, render_template, url_for, session
+import datetime
 
 app = Flask(__name__)
+
+@app.before_request
+def before_request():
+	session.permanent = True
+	app.permanent_session_lifetime = datetime.timedelta(minutes=20)
+	session.modified = True
+
 
 @app.route('/', methods=['GET'])
 def index():
