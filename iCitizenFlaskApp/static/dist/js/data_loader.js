41c8ff46e437e272ef9f3c8b96ad5c7f2a7846ad
Pranay Neelagiri <pneeldam@gmail.com>
2018-01-01 22:08:07 -0500

    removed poll route from js

--- iCitizenFlaskApp/static/dist/js/data_loader.js
+++ iCitizenFlaskApp/static/dist/js/data_loader.js
@@ -1,15 +1,5 @@
 var result1;
 var result2;
-var result3;
-
-$.ajax({
-    url: "/update_polls/",
-    type: "post",
-    success: function(response) {
-    result3 = response
-    $('#poll_test').html(result3)
-    }
-});
 
 $.ajax({
     url: "/load_db/",
