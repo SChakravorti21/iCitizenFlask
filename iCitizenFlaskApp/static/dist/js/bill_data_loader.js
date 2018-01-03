a9643bdf189c2b9e77be2d8bbeb0854120331389
Pranay Neelagiri <pneeldam@gmail.com>
2018-01-03 15:41:55 -0500

    added dynamic html for bills

--- iCitizenFlaskApp/static/dist/js/bill_data_loader.js
+++ iCitizenFlaskApp/static/dist/js/bill_data_loader.js
@@ -10,7 +10,15 @@ function fetchnationalbills() {
             if(response != null){
                 national = response;
                 for (bill of national) {
-                    $('#bill_'+count).html(bill['title']);
+                    var startTag = "<div class=\"card\""
+                    var endTag = "</div>"
+                    var startCardBlock = "<div class=\"card-block\">"
+                    var header = "<div class=\"card-header\">"+bill['level']+"</div>"
+                    var cardTitle = "<h4 class=\"card-title\">"+bill['title']+"</h4>"
+                    var text = "<p class=\"card-text\">"+bill['author']+"</p>"
+                    var info = "<a class=\"btn btn-primary\">"+"More info"+"</a"
+                    var fullText = startTag+header+startCardBlock+cardTitle+text+info+endTag+endTag
+                    $('#bill_'+count).html(fullText);
                     count++;
                 }
             }
@@ -33,7 +41,15 @@ function fetchstatebills() {
             if(response != null){
                 state = response;
                 for (bill of state) {
-                    $('#bill_'+count).html(bill['title']);
+                    var startTag = "<div class=\"card\""
+                    var endTag = "</div>"
+                    var startCardBlock = "<div class=\"card-block\">"
+                    var header = "<div class=\"card-header\">"+bill['level']+"</div>"
+                    var cardTitle = "<h4 class=\"card-title\">"+bill['title']+"</h4>"
+                    var text = "<p class=\"card-text\">"+bill['author']+"</p>"
+                    var info = "<a class=\"btn btn-primary\">"+"More info"+"</a"
+                    var fullText = startTag+header+startCardBlock+cardTitle+text+info+endTag+endTag
+                    $('#bill_'+count).html(fullText);
                     count++;
                 }
             }
