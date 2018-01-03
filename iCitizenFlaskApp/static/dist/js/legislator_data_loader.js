1a79daaa2d7cc428d401b3613798b11679fa1b76
SChakravorti21 <shoumyoarchit@gmail.com>
2018-01-03 15:04:03 -0500

    Legislators displaying nicely

--- iCitizenFlaskApp/static/dist/js/legislator_data_loader.js
+++ iCitizenFlaskApp/static/dist/js/legislator_data_loader.js
@@ -9,17 +9,64 @@ function fetchnationallegs() {
         success: function(response){
             if(response != null){
                 national = response;
+                count = 1;
                 for (legislator of national) {
-                    $('#bill_'+count).html(legislator['title']);
+                    console.log(legislator);
+                    var party = legislator['party'];
+                    if(party === 'D') {
+                        party = 'Democrat';
+                    } else if (party === 'R') {
+                        party = 'Republican';
+                    } else {
+                        party = 'Independent';
+                    }
+
+                    var info = '';
+                    if( legislator['district'] ){
+                        info += 'District: ' + legislator['district'] + '<br><br>';
+                    }
+                    var level = legislator['level'];
+                    info += 'Level: ' + level.charAt(0).toUpperCase() + level.substring(1, level.length).toLowerCase() + '<br><br>';
+                    info += 'Party: ' + party + '<br><br>';
+                    info += 'State: ' + legislator['state'];
+
+                    $('#legislator_national_'+count).html(`
+                        <div class="card mb-4" style='height: 25rem; box-shadow: -5px 5px rgba(120,144,156,0.3);'>
+                            <h4 class="card-header">` + legislator['last_name'] + ', ' + legislator['first_name'] + `</h4>
+                            <div class="card-block">
+                                <div class="row ml-3" style='height: 16rem;'>
+                                    <div class="col-sm-3" 
+                                        style='
+                                            background-image: url(` + legislator['photo_url'] + `);
+                                            background-repeat: no-repeat;
+                                            background-position: center;
+                                            background-size: cover;
+                                            border: 0.25em solid #59698d;
+                                            border-radius: 1em;'>
+                                    </div>
+                                    <div class='col-sm-8 pull-left'>
+                                        <h6 class='card-title'>Chamber: ` + legislator['chamber'] + `</h6><br>
+                                        <p class='card-text'>` + info + `</p>
+                                    </div>
+                                </div>
+                                
+                            </div>
+                            <div class="card-footer text-muted">Legislator ID: ` + legislator['id'] + `</div>
+                            </div>
+                        </div>
+                        `)
                     count++;
                 }
             }
             if(count >= 10) {
-                console.log("National interval has been cleared")
-                clearInterval(nationalInterval)
+                console.log("National legislator interval has been cleared")
+                clearTimeout(nationalInterval)
             } else {
                 console.log("Still polling")
             }
+        },
+        complete: function(response) {
+            setTimeout(nationalInterval, 1000);
         }
     })
 }
@@ -32,21 +79,63 @@ function fetchstatelegs() {
         success: function(response){
             if(response != null){
                 state = response;
+                count = 1;
                 for (legislator of state) {
-                    $('#bill_'+count).html(legislator['title']);
+                    console.log(legislator);
+                    var party = legislator['party'];
+                    var info = '';
+                    if( legislator['district'] ){
+                        info += 'District: ' + legislator['district'] + '<br><br>';
+                    }
+                    var level = legislator['level'];
+                    info += 'Level: ' + level.charAt(0).toUpperCase() + level.substring(1, level.length) + '<br><br>';
+                    info += 'Party: ' + party + '<br><br>';
+                    var state = legislator['state'].toUpperCase();
+                    info += 'State: ' + state.charAt(0).toUpperCase() + state.substring(1, level.length).toLowerCase();
+
+                    var chamber = legislator['chamber'];
+                    chamber = chamber.charAt(0).toUpperCase() + chamber.substring(1, level.length);
+                    $('#legislator_state_'+count).html(`
+                        <div class="card mb-4" style='height: 25rem; box-shadow: -5px 5px rgba(120,144,156,0.3);'>
+                            <h4 class="card-header">` + legislator['last_name'] + ', ' + legislator['first_name'] + `</h4>
+                            <div class="card-block">
+                                <div class="row ml-3" style='height: 16rem;'>
+                                    <div class="col-sm-3" 
+                                        style='
+                                            background-image: url(` + legislator['photo_url'] + `);
+                                            background-repeat: no-repeat;
+                                            background-position: center;
+                                            background-size: cover;
+                                            border: 0.25em solid #59698d;
+                                            border-radius: 1em;'>
+                                    </div>
+                                    <div class='col-sm-8 pull-left'>
+                                        <h6 class='card-title'>Chamber: ` + chamber + `</h6><br>
+                                        <p class='card-text'>` + info + `</p>
+                                    </div>
+                                </div>
+                                
+                            </div>
+                            <div class="card-footer text-muted">Legislator ID: ` + legislator['id'] + `</div>
+                            </div>
+                        </div>
+                        `)
                     count++;
                 }
             }
-            if(count >= 20) {
-                console.log("State interval has been cleared")
-                clearInterval(stateInterval)
+            if(count >= 10) {
+                console.log("State legislator interval has been cleared")
+                clearTimeout(stateInterval)
             } else {
                 console.log("Still polling")
             }
+        },
+        complete: function(response) {
+            setTimeout(stateInterval, 1000);
         }
     });
 }
 $(document).ready(function(){
-    nationalInterval = setInterval(fetchnationallegs, 5000);
-    stateInterval = setInterval(fetchstatelegs, 5000);
+    nationalInterval = setTimeout(fetchnationallegs, 1000);
+    stateInterval = setTimeout(fetchstatelegs, 1000);
 })
